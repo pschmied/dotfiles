17e4e0fb0d63554f106d47d29ced2f0fbbdc270f
Peter Schmiedeskamp <peter@thoughtspot.net>
2019-02-04 13:20:24 -0800

    add xfsettingsd to startup

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -10,15 +10,13 @@ main = do
 
   xmproc <- spawnPipe "/usr/bin/xautolock -time 15 -locker slock"
   xmproc <- spawnPipe "/usr/bin/xsetroot -solid lightblue"
-  -- xmproc <- spawnPipe "/usr/local/bin/xmobar ~/.xmobarrc"
   xmproc <- spawnPipe "/usr/bin/setxkbmap -option ctrl:nocaps -option compose:ralt"
   xmproc <- spawnPipe "/usr/bin/gpg-agent --enable-ssh --daemon"
   xmproc <- spawnPipe "/usr/bin/synclient palmdetect=1 MaxTapTime=0"
   xmproc <- spawnPipe "/usr/bin/numlockx on"
-  -- xmproc <- spawnPipe "/usr/bin/emacs --daemon"
+  xmproc <- spawnPipe "/usr/bin/xfsettingsd"
   xmproc <- spawnPipe "/usr/bin/xfce4-volumed"
   xmproc <- spawnPipe "/usr/bin/xfce4-power-manager"
-  -- xmproc <- spawnPipe "/usr/bin/mixerctl  outputs.master=156"
 
   xmonad $ baseConfig
     { modMask = mod4Mask
