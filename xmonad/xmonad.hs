91a8de273f3f855f0fef8cb049b17afdad73c359
Peter Schmiedeskamp <peter@thoughtspot.net>
2019-02-08 15:56:52 -0800

    change locker to xflock4; add boilerplate for network-monitor applet and panel

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,7 +8,7 @@ baseConfig = desktopConfig
 
 main = do
 
-  xmproc <- spawnPipe "/usr/bin/xautolock -time 15 -locker slock"
+  xmproc <- spawnPipe "/usr/bin/xautolock -time 15 -locker xflock4"
   xmproc <- spawnPipe "/usr/bin/xsetroot -solid lightblue"
   xmproc <- spawnPipe "/usr/bin/setxkbmap -option ctrl:nocaps -option compose:ralt"
   xmproc <- spawnPipe "/usr/bin/gpg-agent --enable-ssh --daemon"
@@ -17,6 +17,8 @@ main = do
   xmproc <- spawnPipe "/usr/bin/xfsettingsd"
   xmproc <- spawnPipe "/usr/bin/xfce4-volumed"
   xmproc <- spawnPipe "/usr/bin/xfce4-power-manager"
+  -- xmproc <- spawnPipe "/usr/bin/nm-applet"
+  -- xmproc <- spawnPipe "/usr/bin/xfce4-panel"
 
   xmonad $ baseConfig
     { modMask = mod4Mask
